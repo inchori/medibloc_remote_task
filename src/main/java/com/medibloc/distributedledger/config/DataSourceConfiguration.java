180c3d04abd925919616521da4190f508a88d026
InChul <49394875+inchori@users.noreply.github.com>
2021-11-02 17:58:33 +0900

    fix: Replace Mysql Port Number

---
+++ src/main/java/com/medibloc/distributedledger/config/DataSourceConfiguration.java
@@ -0,0 +1,59 @@
+package com.medibloc.distributedledger.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DataSourceConfiguration {
+    public static final String MASTER_DATASOURCE = "masterDataSource";
+    public static final String SLAVE_DATASOURCE = "slaveDataSource";
+
+    @Bean(name = MASTER_DATASOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+    public DataSource masterDatasource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean(name = SLAVE_DATASOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
+    public DataSource slaveDatasource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @Primary
+    @DependsOn({MASTER_DATASOURCE, SLAVE_DATASOURCE})
+    public DataSource routingDataSource(@Qualifier(MASTER_DATASOURCE) DataSource masterDataSource,
+                                        @Qualifier(SLAVE_DATASOURCE) DataSource slaveDataSource) {
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+        Map<Object, Object> dataSourceMap = new HashMap<>() {
+            {
+                put("master", masterDataSource);
+                put("slave", slaveDataSource);
+            }
+        };
+
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+
+        return routingDataSource;
+    }
+
+    @Bean
+    @DependsOn(value = "routingDataSource")
+    public LazyConnectionDataSourceProxy dataSource(DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
--- src/main/java/com/medibloc/distributedledger/service/UserAccountService.java
+++ src/main/java/com/medibloc/distributedledger/service/UserAccountService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
+
 @RequiredArgsConstructor
 @Service
 @Transactional
